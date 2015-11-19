4f987e4ad548e7e5fbb6fc8f7dfd6aca310f7f3f
Rodrigo Wirth <rodrigo.wirth@bravi.com.br>
2015-11-19 13:58:26 -0200

    Refactor the code

--- bomboneiro.ino
+++ bomboneiro.ino
@@ -5,101 +5,77 @@
 #define lightPin A5
 HX711 scale(A1, A0);
 
+
 const int foutainPin = 13;
 const bool replacing = false;
+
+//Times
 const long initialDelayForStartReading = 60000;//60 seconds
 const long alarmDuration = 15000;//15 seconds
 const long changeGaloonWaitingTime = 600000;//10 minutes
 int alarmCount = 0;
 
-void setup() {
-  // put your setup code here, to run once:
- Serial.begin(9600);
+/*
+    FOUNTAIN: Liga e desliga o refrigerados de agua
+    LED: Liga e desliga o giroflex
+*/
 
+void setup() {
+  Serial.begin(9600);
+
+  //Defines pin modes
   pinMode(ledPin, OUTPUT);
   pinMode(foutainPin, OUTPUT);
   pinMode(lightPin, INPUT);
 
-  pinMode(foutainPin, HIGH);
+  //Start fountain and led ligados
+  digitalWrite(foutainPin, HIGH);
   digitalWrite(ledPin, HIGH);
-  
-  Serial.println("HX711 Demo");
-  Serial.println("Before setting up the scale:");
-  Serial.print("read: \t\t ");
-  Serial.println(scale.read());      // print a raw reading from the ADC 
-  
-  Serial.print("read average:\t\t ");
-  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-  
-  Serial.print("get value: \t\t ");
-  Serial.println(scale.get_value(5));   
-// print the average of 5 readings from the ADC minus the tare weight (not set yet)
- 
-  Serial.print("get units: \t\t ");
-  Serial.println(scale.get_units(5), 1);  
-// print the average of 5 readings from the ADC minus tare weight (not set) divided by the SCALE parameter (not set yet) </p><p>  scale.set_scale(2280.f);  
- // this value is obtained by calibrating the scale with known weights; see the README for details
-  scale.set_scale(20500);
-  scale.tare();   // reset the scale to 0
-    
- Serial.println("After setting up the scale:");
- 
- Serial.print("read: \t\t");
- Serial.println(scale.read());                        // print a raw reading from the ADC
- 
- Serial.print("read average:\t\t ");
- Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
- 
- Serial.print("get value: \t\t ");    
-// print the average of 5 readings from the ADC minus the tare weight, set with tare()
-Serial.println(scale.get_value(5));
-Serial.print("get units: ");
-Serial.println(scale.get_units(5), 1);      
-// print the average of 5 readings from the ADC minus tare weight, divided by the SCALE parameter set with set_scale
- 
-Serial.println("Readings:");
 
-digitalWrite(ledPin, LOW); 
-delay(initialDelayForStartReading);
+  //Setup the scale  
+  scale.set_scale(20500);
+  scale.tare();
+
+  //Turn the led off
+  digitalWrite(ledPin, LOW); 
   
+  //Delay before start reading
+  delay(initialDelayForStartReading);
 }
 
 void loop() 
 {
-  
-  /*Serial.print("get units after tare: \t\t ");
-  Serial.println(scale.get_units(5));
-  Serial.print("read: \t\t ");
-  Serial.println(scale.read()); 
-  delay(2000);*/
-  Serial.print("Weight :");                        
-  /* ---------Weight in terms of KG-------------*/
-  double value = scale.get_units(20)*1;
+  //Gets the weight in kg
+  double value = scale.get_units(20) * 1; 
+  Serial.print("Weight :");
   Serial.print(value , 2);
-  Serial.println("KG  \t\t ");
-  
+  Serial.println("KG");
+
+  //Turn on the giroflex if it should to
   if (value < 0.3)
   {
     alarmCount ++;
-    if(alarmCount >3)
+    if(alarmCount > 3)
     {
+      Serial.println("Giroflex turned on");
       digitalWrite(ledPin, HIGH);
-      Serial.println("Start Alarm!! ");
       delay(alarmDuration);
-      
       digitalWrite(ledPin, LOW); 
-      Serial.println("Start Waiting time...");
-      delay(changeGaloonWaitingTime);
+      Serial.println("Giroflex turned off");
       
-      alarmCount =0;
+      Serial.println("Start waiting time...");
+      delay(changeGaloonWaitingTime);
+
+      alarmCount = 0;
     }
   }
   else
   {
-    alarmCount =0;
     digitalWrite(ledPin, LOW);
+    alarmCount = 0;
   }
 
+  //Turn on or off the fountain
   double light = analogRead(lightPin);
   Serial.println("light  \t\t ");
   Serial.print(light , 2);
@@ -113,10 +89,11 @@ void loop()
     Serial.println("Turn off fountain!! ");
     digitalWrite(foutainPin,LOW);
   }
-  
-  
-  scale.power_down();             // put the ADC in sleep mode
+
+  //Put the ADC in sleep mode
+  scale.power_down();
   delay(1000);
   scale.power_up();  
- 
+
 }
+
